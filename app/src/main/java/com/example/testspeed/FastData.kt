80c06860db2353d30d9495c4c6e47c82050a2537
umedqws <120251717+umedqws@users.noreply.github.com>
2023-04-18 09:04:30 +0300

    finished

--- app/src/main/java/com/example/testspeed/FastData.kt
+++ app/src/main/java/com/example/testspeed/FastData.kt
@@ -1,34 +1,21 @@
-import android.os.AsyncTask
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.util.concurrent.TimeUnit
+package com.example.testspeed
 
-class SpeedTestTask : AsyncTask<Void?, Void?, String>() {
 
-    override fun doInBackground(vararg params: Void?): String {
-        var speed = 0.0
-        try {
-            val url = "https://speedtest.net/"
-            val client = OkHttpClient.Builder().build()
-            val request = Request.Builder()
-                .url(url)
-                .build()
-            var totalTime = 0L
-            var totalSize = 0L
-            repeat(3) {
-                val startTime = System.nanoTime()
-                val response = client.newCall(request).execute()
-                response.body?.byteStream()?.readBytes()
-                val endTime = System.nanoTime()
-                val elapsedTime = endTime - startTime
-                totalTime += elapsedTime
-                totalSize += response.body?.contentLength() ?: 0L
-                response.close()
-            }
-            speed = (totalSize.toDouble() / 1024.0 / 1024.0) / ((totalTime.toDouble() / 1000000000.0) / 3)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return String.format("%.2f", speed)
-    }
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+data class SpeedTestData(
+     val speedValue: String,
+     val latencyValue: String,
+     val bufferBloatValue: String,
+     val uploadValue: String
+)
+
+
+interface SpeedTestService {
+     @POST(".")
+    suspend fun postSpeedTestData(@Body data: SpeedTestData): Response<ResponseBody>
+
 }
